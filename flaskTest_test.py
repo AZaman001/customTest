b2bc56ffdfd9834a8b145c87c90abf6e838b9c04
Annadil Zaman <ahzaman-oracle@Annadils-MacBook-Pro.local>
2019-02-01 14:36:27 -0500

    /dadjoke NOT /dadjokes

--- flaskTest_test.py
+++ flaskTest_test.py
@@ -7,7 +7,7 @@ class JokesTestCase(unittest.TestCase):
 
   def test_index(self):
     tester = app.test_client(self)
-    response = tester.get('/dadjokes', content_type='text/plain')
+    response = tester.get('/dadjoke', content_type='text/plain')
     self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
