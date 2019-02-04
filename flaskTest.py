b8ee40c7ecd1ff2a5e03853e178e89fc787b2019
Annadil Zaman <ahzaman-oracle@Annadils-MacBook-Pro.local>
2019-02-04 11:38:42 -0500

    added functionality to test pipeline

--- flaskTest.py
+++ flaskTest.py
@@ -4,7 +4,7 @@ import requests
 app = Flask(__name__)
 
 @app.route('/dadjoke')
-def root():
+def dad():
     resp = requests.get('https://icanhazdadjoke.com/', headers={'Accept':'text/plain'})
 
     if resp.status_code != 200:
@@ -12,6 +12,17 @@ def root():
         raise ApiError('GET /tasks/ {}'.format(resp.status_code))
     if  resp.status_code == 200:
         theJoke = resp.content.decode("utf-8")
+    return theJoke
+
+@app.route('/advice')
+def advice():
+    resp = requests.get('http://api.adviceslip.com/advice', headers={'Accept':'application/json'})
+
+    if resp.status_code != 200:
+        # This means something went wrong.
+        raise ApiError('GET /tasks/ {}'.format(resp.status_code))
+    if  resp.status_code == 200:
+        theJoke = resp.content
     return theJoke
 
 if __name__ == '__main__':
--- flaskTest_test.py
+++ flaskTest_test.py
@@ -3,12 +3,19 @@ from flaskTest import app
 import unittest
 import json
 
+# I understand that these aren't thorough tests
 class JokesTestCase(unittest.TestCase):
 
-  # I understand that this isnt' a thorough test
+  # dad joke route
   def test_index(self):
     tester = app.test_client(self)
     response = tester.get('/dadjoke', content_type='text/plain')
+    self.assertEqual(response.status_code, 200)
+
+  # advice route
+  def test_index(self):
+    tester = app.test_client(self)
+    response = tester.get('/advice', content_type='application/json')
     self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
